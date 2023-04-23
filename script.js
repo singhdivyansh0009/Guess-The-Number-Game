2a7b0caa6697da259d645bf6aa547090395da905
Divyansh Kumar Singh <102811377+singhdivyansh0009@users.noreply.github.com>
2023-04-24 02:06:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+  let randomNumber = getRandomInt(1, 100);   
+  let guessedNumber;
+  let count = 0;
+  const compute = () => {
+    count++;
+    guessedNumber = document.getElementById("guess").value;
+    if (guessedNumber < randomNumber) {
+        document.getElementById("score").innerHTML = ``;
+        document.getElementById("ans").innerHTML = "The number is Greater than you guessed";
+    } else if (guessedNumber > randomNumber) {
+        document.getElementById("ans").innerHTML = "The number is Smaller than you guessed";
+    } else {
+        document.getElementById("ans").innerHTML="<b>CONGRATULATION</b>"
+        document.getElementById("score").innerHTML = `Your score is ${100 - count}`;
+        randomNumber = getRandomInt(1, 100);
+        count=0;
+    }
+}
+ 
